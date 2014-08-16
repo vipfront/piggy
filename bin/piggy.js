8bf7f64f836254ab93cd0531d4c8170d2fa0a37e
tom.wang <tom.wang.sz@qq.com>
2014-08-16 13:41:38 +0800

    添加日志记录支持

--- bin/piggy.js
+++ bin/piggy.js
@@ -1,4 +1,20 @@
 #!/usr/bin/env node
 
 var piggy = require('../lib/main.js')
+
+// 记录错误日志
+piggy.on('error', function(e) {
+    log(e)
+})
+
+process.on('uncaughtException', function(e) {
+    log(e)
+})
+
+function log(e) {
+    piggy.logger.error(e.stack || e.message)
+    console.error(e.stack || e.message)
+    process.exit(e.code || 1)
+}
+
 piggy.init()
